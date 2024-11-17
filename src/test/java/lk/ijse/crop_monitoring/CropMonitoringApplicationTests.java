8f757687bcfd2c20cff21b6a04ecfc8b0168f041
Ashinshana <ashinshananavodi320@gmail.com>
2024-11-17 12:53:28 +0530

    Added initial project files

---
+++ src/test/java/lk/ijse/crop_monitoring/CropMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.crop_monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CropMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
