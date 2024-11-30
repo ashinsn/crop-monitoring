8cd08d9f841f452d0f107a9bb6b903303d0b923a
Ashinshana <ashinshananavodi320@gmail.com>
2024-11-30 22:54:26 +0530

    create the Custom interface

--- src/main/java/lk/ijse/crop_monitoring/service/UserService.java
+++ src/main/java/lk/ijse/crop_monitoring/service/UserService.java
@@ -1,8 +1,12 @@
 package main.java.lk.ijse.crop_monitoring.service;
 
 import main.java.lk.ijse.crop_monitoring.entity.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UserService {
+public interface UserService extends UserDetailsService{
+    // Add any additional methods if needed
+    // Example:
+    void registerUser(String username, String password, String role);
     User saveUser(User user);
     User getUserByUsername(String username);
 }
