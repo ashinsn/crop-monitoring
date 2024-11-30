8937bcdb6bdc2ee92955be9c5e7404dc3b1c156d
Ashinshana <ashinshananavodi320@gmail.com>
2024-11-30 21:46:13 +0530

    Role entity

---
+++ src/main/java/lk/ijse/crop_monitoring/entity/Role.java
@@ -0,0 +1,14 @@
+package main.java.lk.ijse.crop_monitoring.entity;
+import jakarta.*;
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    // Getters and Setters
+}
