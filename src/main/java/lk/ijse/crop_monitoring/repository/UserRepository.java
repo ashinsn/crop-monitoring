ec0c719af12414534e0cdbf12df5edb66d1854ea
Ashinshana <ashinshananavodi320@gmail.com>
2024-11-30 21:56:48 +0530

    create UserRepository

---
+++ src/main/java/lk/ijse/crop_monitoring/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package main.java.lk.ijse.crop_monitoring.repository;
+
+import com.crop_monitoring.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
