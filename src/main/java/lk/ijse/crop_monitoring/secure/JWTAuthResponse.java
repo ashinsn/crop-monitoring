95c2b1fe5699aa2ed10386b3aed2aea47fa66c7a
Ashinshana <ashinshananavodi320@gmail.com>
2024-12-05 10:35:29 +0530

    implement secure

---
+++ src/main/java/lk/ijse/crop_monitoring/secure/JWTAuthResponse.java
@@ -0,0 +1,14 @@
+package main.java.lk.ijse.crop_monitoring.secure;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class JWTAuthResponse {
+    private String token;
+}
---
+++ src/main/java/lk/ijse/crop_monitoring/secure/SignIn.java
@@ -0,0 +1,15 @@
+package main.java.lk.ijse.crop_monitoring.secure;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class SignIn {
+    private String email;
+    private String password;
+}
