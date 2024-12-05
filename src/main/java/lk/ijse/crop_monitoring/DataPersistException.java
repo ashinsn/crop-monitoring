51d8418edf4c63dda2bc61384becbb8a78b68481
Ashinshana <ashinshananavodi320@gmail.com>
2024-12-05 11:21:08 +0530

    add DataPersistException

---
+++ src/main/java/lk/ijse/crop_monitoring/DataPersistException.java
@@ -0,0 +1,13 @@
+package main.java.lk.ijse.crop_monitoring;
+
+public class DataPersistException extends RuntimeException {
+    public DataPersistException() {
+    }
+
+    public DataPersistException(String message) {
+
+    }
+    public DataPersistException(String message, Throwable cause) {
+
+    }
+}
